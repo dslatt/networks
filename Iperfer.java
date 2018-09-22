78af15dbf353d53200dfe8b2e8c4f889f0e54c91
Dylan Slattery <slattery@royal-28.cs.wisc.edu>
2018-09-22 15:41:22 -0500

    converted to simple parsing method

--- Iperfer.java
+++ Iperfer.java
@@ -11,49 +11,88 @@ import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-public class Iperfer implements Runnable {
+public class Iperfer {
 
     /* constant values */
     public static final int BLOCK_SIZE = 1000;
     public static final int MIN_PORT = 1024;
     public static final int MAX_PORT = 65535;
 
-    @Option(names="-c", description="run Iperfer in client mode")
-    private boolean clientMode = false;
+    private static boolean badArgs = false;
+    private static boolean badArgsFormat = false;
 
-    @Option(names="-s", description="run Iperfer in server mode")
-    private boolean serverMode = false;
+    private static int mode = 0;
 
-    @Option(names="-p", arity="1", description="port to use for connection (Must be in range 1024-65535)")
-    private int port = 0;
+    private static int port = 0;
+    private static String host = "";
+    private static long time = 0;
 
-    @Option(names="-h", arity="1", description="host IP address to connect to")
-    private String host = "";
+    private static void parseSimpleArgsList(String args[]) {
+        if (args[0].equals("-c")){
+            mode = 0;
+            if (args.length == 7) { 
+                if (!args[3].equals("-p") || 
+                    !args[1].equals("-h") || 
+                    !args[5].equals("-t"))
+                {
+                    badArgs = true;
+                }else{
+                    try {
+                        port = Integer.parseInt(args[4]);
+                        time = Long.parseLong(args[6]);
+                        host = args[2];
+                    }catch(NumberFormatException e){
+                        badArgsFormat = true;
+                    }
+                }
+            } else{
+                badArgs = true;
+            }
+        } else if (args[0].equals("-s")){
+            mode = 1;
+            if (args.length == 3) {
+                if(!args[1].equals("-p")) {
+                    badArgs = true;
+                }else{
+                    try {
+                        port = Integer.parseInt(args[2]);
+                    }catch(NumberFormatException e){
+                        badArgsFormat = true;
+                    }
+                }
+            }else {
+                badArgs = true; 
+            }
+        }else{
+            System.out.printf("Error: Must specify mode (client '-c' or server -s')%n");
+            System.exit(1);
+        }
+        if (badArgs) {
+            System.out.printf("Error: missing or additional arguments%n");
+            System.exit(1);
+        }else if(badArgsFormat){
+            System.out.printf("Error: invalid argument%n");
+            System.exit(1);
+        }
+    }
 
-    @Option(names="-t", arity="1", description="time period to test")
-    private long time = 0;
+    public static void main(String args[]) {
 
-    public void run() {
+        parseSimpleArgsList(args);
 
         if (port < MIN_PORT || port > MAX_PORT) {
             System.out.printf("Error: port number must be in the range %d to %d%n", MIN_PORT, MAX_PORT);
             System.exit(1);
         }
 
-        if(clientMode){
+        if(mode == 0){
             Client client = new Client(port, host, time);
             client.start();
             client.printResults();
-        }else if(serverMode){
+        }else if(mode == 1){
             Server server = new Server(port);
             server.start();
             server.printResults();
-        }else{
-            System.out.printf("Error: Must specify mode (client '-c' or server -s')%n");
         }
-    }
-
-    public static void main(String[] args){
-        CommandLine.run(new Iperfer(), args);
     }
 }
